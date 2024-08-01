51be18dc9df50049044334e6361c86acaa068e33
Saf-05 <safeyaalbraiki@gmail.com>
2024-08-01 08:15:29 +0100

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,9 @@
+public class Hello{
+
+    public static void main( String[] args){
+
+
+        System.out.println("Author: Safiya");
+        System.out.println("Jenkins Project - Explaining Git Project Compilation");
+    }
+}
